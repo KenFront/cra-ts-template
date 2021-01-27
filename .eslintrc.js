a518d110dc991dca2eb8d9f5139212f65182d3cc
ken.wang <ken.wang@pentium.network>
2021-01-27 14:15:22 +0800

    init coding style setting

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "react-app",
+    "react-app/jest",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+};
